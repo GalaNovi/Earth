5d7971a636edf0ac8ce5636d854d597525f13d51
Darkmus <darkmus@tut.by>
2018-12-19 11:26:54 +0300

    Complete slider for the gallery

---
+++ src/js/modules/sliders.js
@@ -0,0 +1,46 @@
+// Слайдер в галерее
+
+$('.gallery__list').slick({
+  accessibility: false,
+  infinite: false,
+  prevArrow: '<button class="gallery__list-arrow gallery__list-arrow--previous" type="button"><span class="visually-hidden">Предыдущий слайд</span></button>',
+  nextArrow: '<button class="gallery__list-arrow gallery__list-arrow--next" type="button"><span class="visually-hidden">Следующий слайд</span></button>',
+  asNavFor: '.gallery__list-text'
+});
+
+$(".gallery__list-text").on("init", function(event, slick) {
+  if (slick.currentSlide + 1 < 10) {
+    $(".gallery__current-item").text('0' + parseInt(slick.currentSlide + 1));
+  } else {
+    $(".gallery__current-item").text(parseInt(slick.currentSlide + 1));
+  };
+  if (slick.slideCount < 10) {
+    $(".gallery__all-items").text('0' + parseInt(slick.slideCount));
+  } else {
+    $(".gallery__all-items").text(parseInt(slick.slideCount));
+  };
+});
+
+$(".gallery__list-text").on("afterChange", function(event, slick, currentSlide) {
+  if (slick.currentSlide + 1 < 10) {
+    $(".gallery__current-item").text('0' + parseInt(slick.currentSlide + 1));
+  } else {
+    $(".gallery__current-item").text(parseInt(slick.currentSlide + 1));
+  };
+  if (slick.slideCount < 10) {
+    $(".gallery__all-items").text('0' + parseInt(slick.slideCount));
+  } else {
+    $(".gallery__all-items").text(parseInt(slick.slideCount));
+  };
+});
+
+$('.gallery__list-text').slick({
+  fade: true,
+  accessibility: false,
+  infinite: false,
+  arrows: false,
+  asNavFor: '.gallery__list'
+});
+
+
+// Слайдер в блоке "Сколько это в цифрах"
